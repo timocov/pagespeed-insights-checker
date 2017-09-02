0a573e9a213c18f37be1fee0c3f62b0d1db4b9a8
Eugene Timokhov <timocov@gmail.com>
2017-09-02 22:51:58 +0300

    Added saving/restoring state
    - State will be used between runs to detect changes
    - If page has no changes in scores - no messages for page will be generated

--- README.md
+++ README.md
@@ -16,5 +16,7 @@ export GOOGLE_API_KEY='GOOGLE_API_KEY'
 export SLACK_INCOMING_KEY='T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX'
 export SLACK_CHANNEL='SLACK_CHANNEL' # e.g. '#general' or '@someone'
 
+export STATE_FILE='file.json' # will be used to restore/save state between runs
+
 python main.py https://google.com https://stackoverflow.com/ https://github.com/
 ```
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import json
 import sys
 import requests
 
@@ -10,13 +11,13 @@ SLACK_API_URL = 'https://hooks.slack.com/services/'
 GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY', None)
 SLACK_INCOMING_KEY = os.environ.get('SLACK_INCOMING_KEY', None)
 SLACK_CHANNEL = os.environ.get('SLACK_CHANNEL', None)
+STATE_FILE = os.environ.get('STATE_FILE', None)
+
 
 class WrongResponseCode(Exception):
     def __init__(self, code):
-        self._code = code
-
-    def __str__(self):
-        return 'Page returns {0} code'.format(self._code)
+        Exception.__init__(self)
+        self.code = code
 
 
 def request_score_for_strategy(url, strategy):
@@ -66,26 +67,87 @@ def send_to_slack(message):
     print('Slack response: {0}'.format(response.content))
 
 
+def generate_pagespeed_slack_url(page_url):
+    return '<{0}?url={1}|{1}>'.format(PAGESPEED_URL, page_url)
+
+
+def generate_scores_slack_message(scores, changes):
+    desktop_str = ':computer: - *{desktop}*/100 ({desktop_change:+})'.format(**scores, **changes)
+    mobile_str = ':iphone: - *{mobile}*/100 ({mobile_change:+})'.format(**scores, **changes)
+    return '{0} {1}'.format(desktop_str, mobile_str)
+
+
+def get_changes(prev_scores, new_scores):
+    # prev state might be empty but not new
+    desktop_change = new_scores.get('desktop') - prev_scores.get('desktop', 0)
+    mobile_change = new_scores.get('mobile') - prev_scores.get('mobile', 0)
+
+    if desktop_change == 0 and mobile_change == 0:
+        return None
+
+    return {
+        'desktop_change': desktop_change,
+        'mobile_change': mobile_change,
+    }
+
+
+def load_prev_state():
+    if STATE_FILE is not None and os.path.exists(STATE_FILE):
+        with open(STATE_FILE) as file:
+            return json.load(file)
+    return {}
+
+
+def save_state(new_state):
+    if STATE_FILE is None:
+        print('Cannot save state - you must specify state file')
+        return
+
+    with open(STATE_FILE, 'w') as file:
+        file.write(json.dumps(new_state))
+
+    print('State saved to file "{0}"'.format(STATE_FILE))
+
+
 def main():
     urls = sys.argv[1:]
     assert len(urls) > 0, 'Urls must be specified'
-    print('Generating report for pages: {0}\n'.format(', '.join(urls)))
+    print('Generating report for pages: {0}'.format(', '.join(urls)))
 
-    message = ['*PageSpeed Insights* report:']
+    prev_state = load_prev_state()
+    print('Used saved state: {0}'.format(json.dumps(prev_state)))
 
+    new_state = prev_state.copy()
+    need_send_message_to_slack = False
+
+    slack_message_parts = ['*PageSpeed Insights* report:']
     for url in urls:
-        print('Generate report for {0}'.format(url))
-        report_string = None
+        print('Generating report for {0}'.format(url))
+
+        page_report_str = None
         try:
             scores = request_scores(url)
-            report_string = ':computer: - *{desktop}*/100 :iphone: - *{mobile}*/100'.format(**scores)
+            changes = get_changes(prev_state.get(url, {}), scores)
+            if changes is None:
+                print('  Scores for page is not changed')
+                continue
+
+            new_state[url] = scores
+            page_report_str = generate_scores_slack_message(scores, changes)
+            print('  Desktop={desktop}/100 ({desktop_change:+}), mobile={mobile}/100 ({mobile_change:+})'.format(**scores, **changes))
         except WrongResponseCode as error:
-            report_string = '`{0}` :bangbang:'.format(error)
+            page_report_str = '`Page returns {0}` :bangbang:'.format(error.code)
+            print('  Cannot generate report - page returns {0} code'.format(error.code))
 
-        print('  Scores: {0}'.format(report_string))
-        message.append('<{0}?url={1}|{1}> {2}'.format(PAGESPEED_URL, url, report_string))
+        need_send_message_to_slack = True
+        pagespeed_link = generate_pagespeed_slack_url(url)
+        slack_message_parts.append('{0} {1}'.format(pagespeed_link, page_report_str))
 
-    send_to_slack('\n'.join(message))
+    save_state(new_state)
+    if need_send_message_to_slack:
+        send_to_slack('\n'.join(slack_message_parts))
+    else:
+        print('Skip sending message to slack because of no changes for all pages')
 
 
 if __name__ == '__main__':
